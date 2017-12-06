22f945646f20fd284081afcfcacbb10a3b614eb3
tpin3694 <t.pinder@student.reading.ac.uk>
2017-12-06 18:38:47 +0000

    RF Accuracy Plots

--- R/random_forest.R
+++ R/random_forest.R
@@ -1,16 +1,19 @@
 library(data.table)
 library(randomForest)
 library(caret)
+library(mlbench)
+library(pROC)
+library(dplyr)
 
 setwd("~")
 setwd("Documents/university/MSc/fundamental/fraud/")
 
 # Read in Data
-site_data <- fread("data/stratified/dataset_119.csv")
+site_data <- fread("data/stratified/dataset_121.csv")
 fraud <- fread("data/stratified/fraud_data.csv")
 
 # Append DataFrames
-data <- rbind(site_data, fraud)
+data <- unique(rbind(site_data, fraud))
 
 # Convert Status to Factor
 data$fraud_status <- as.factor(data$fraud_status)
@@ -26,61 +29,48 @@ train_data <- data[index, ]
 test_data <- data[-index, ]
 table(data$fraud_status)
 
-# Setup Random Forest
-model_rf <- train(fraud_status ~ ., 
-                  data = data,
-                  preProcess = c("scale", "center"),
-                  trControl = trainControl(method = "repeatedcv",
-                                           number = 10,
-                                           repeats = 3,
-                                           verboseIter = TRUE))
+# RF Using Caret
+# Number = Repeats = 10
+control <- trainControl(method = "repeatedcv", number = 2, repeats = 1)
+metric <- "Accuracy"
+mtry <- sqrt(ncol(data))
+tunegrid <- expand.grid(.mtry=mtry)
+rf_default <- train(fraud_status ~ .,
+                    data = train_data,
+                    method = "rf",
+                    metric = metric,
+                    tuneGrid = tunegrid,
+                    trControl = control)
 
-final <- data.frame(actual = test_data$fraud_status, 
-                    predict(model_rf, newdata = test_data, type = "prob"))
-# final$predict <- ifelse(final$benign > 0.5, "benign", "malignant")
-# cm_original <- confusionMatrix(final$predict, test_data$classes)
+to_test <- test_data %>% 
+  select(-fraud_status)
 
-# Under Sample now
-ctrl <- trainControl(method = "repeatedcv", 
-                     number = 2, 
-                     repeats = 2,#10 
-                     verboseIter = FALSE,
-                     sampling = "smote")
+prediction <- predict(rf_default, newdata = test_data, type = "prob")
+prediction_val <- ifelse(prediction$`0`>0.5, 0, 1)
+results <- data.frame(actual = test_data$fraud_status, prediction = prediction_val)
+results$result <- ifelse(results$actual==results$prediction, 1, 0)
+sum(results$result)/dim(results)[1]
 
-set.seed(123)
-nmin <- sum(train_data$fraud_status == 1)
-model_rf_under <- train(fraud_status ~ .,
-                        data = train_data,
-                        method = "rf",
-                        preProcess = c("scale", "center"),
-                        trControl = ctrl,
-                        strata = train_data$fraud_status,
-                        sampsize = rep(nmin, 2))
 
-final_under <- data.frame(actual = test_data$fraud_status, 
-                          predict(model_rf_under, newdata = test_data, 
-                                  type = "prob"))
-
-final_under$predict <- ifelse(final_under$X0 < 0.5, 0, 1)
-cm_under <- confusionMatrix(final_under$predict, test_data$fraud_status)
-cm_under
-# Plot Model
-plot(rf_model, ylim = c(0, 0.99))
-legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
-
-# Variable Importance
-var_importance <- importance(rf_model)
-varImportance <- data.frame(Variables = row.names(var_importance), 
-                            Importance = round(var_importance[ ,'MeanDecreaseGini'],2))
-rankImportance <- varImportance %>%
-  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
-ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
-                           y = Importance, fill = Importance)) +
-  geom_bar(stat='identity') + 
-  geom_text(aes(x = Variables, y = 0.5, label = Rank),
-            hjust=0, vjust=0.55, size = 4, colour = 'red') +
-  labs(x = 'Variables') +
-  coord_flip() + 
-  theme_minimal()
-plot(cm_under$table)
-cm_under$table
+# #Try Original RF
+# rf_model <- randomForest(fraud_status ~ . , data = train_data)
+# plot(rf_model)
+# print(rf_model$confusion)
+# legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
+# 
+# # Variable Importance
+# var_importance <- importance(rf_model)
+# varImportance <- data.frame(Variables = row.names(var_importance), 
+#                             Importance = round(var_importance[ ,'MeanDecreaseGini'],2))
+# # Plot Variable Rank
+# rankImportance <- varImportance %>%
+#   mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+# ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+#                            y = Importance, fill = Importance)) +
+#   geom_bar(stat='identity') + 
+#   geom_text(aes(x = Variables, y = 0.5, label = Rank),
+#             hjust=0, vjust=0.55, size = 4, colour = 'red') +
+#   labs(x = 'Variables') +
+#   coord_flip() + 
+#   theme_minimal()
+# 
--- py/fraud_rf.py
+++ py/fraud_rf.py
@@ -1,17 +1,101 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
+from sklearn.metrics import accuracy_score, confusion_matrix
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
 
-thg_data = pd.read_csv("/home/tpin3694/Documents/university/MSc/fundamental/hot_pot_encoded.csv",
-                       parse_dates = True, encoding = "ISO-8859-1")
+def concate_df(list_of_df):
+    to_model = pd.concat(list_of_df, ignore_index=True)
+    to_model = to_model.drop("Site_Key", 1)
+    to_model = to_model.drop_duplicates()
+    print("Shape: " + str(to_model.shape))
+    return to_model
 
-# Split Up Dataset
-thg_features = thg_data.loc[:, thg_data.columns != "fraud_status"].astype(int)
-thg_target = thg_data['fraud_status'].astype(int)
 
-# Build the Random Forest Classifier
-recogniser = RandomForestClassifier(25)
+def nan_checker(dataframe):
+    num_nan = dataframe.isnull().sum()
+    total = np.sum(num_nan)
+    return total
 
-# Fit Data
-recogniser.fit(thg_features, thg_target)
 
+def training_splitter(dataframe, data_split):
+    train_x, test_x, train_y, test_y = train_test_split(dataframe.loc[:, dataframe.columns != "fraud_status"],
+                                                        dataframe['fraud_status'], train_size=data_split)
+    return train_x, test_x, train_y, test_y
+
+
+def random_forest_train(features, label, no_trees):
+    clf = RandomForestClassifier()
+    clf.fit(features, label)
+    return clf
+
+
+def random_forest_predict(model, features):
+    results = model.predict(features)
+    return results
+
+
+def cross_validation(model, features, label, k, score):
+    cv_score = cross_validate(model, features, label, cv = k, scoring=score)
+    return cv_score
+
+
+def plotter(scores, array1, array2, tree_list, dir, file):
+    plt.plot(tree_list, scores)
+    plt.plot(tree_list, array1 + array2, 'b--')
+    plt.plot(tree_list, array1 - array2, 'b--')
+    plt.ylabel('CV score')
+    plt.xlabel('# of trees')
+    plt.savefig(dir + 'accuracy_plots/dataset'+str(file).strip(file_dir)+".png")
+
+file_dir = "/home/tpin3694/Documents/university/MSc/fundamental/fraud/data/stratified/"
+fraud = pd.read_csv(file_dir+"fraud_data.csv", usecols = range(51))
+files = glob.glob(file_dir+"dataset*.csv")
+
+
+for file in files:
+    print("Working on dataset" + str(file).strip(file_dir), sep = "")
+    data = pd.read_csv(file, encoding = "ISO-8859-1")
+    to_model = concate_df([data, fraud])
+    nan_count = nan_checker(to_model)
+    if nan_count > 0:
+        print("Number of NaN: " + str(nan_count))
+    else:
+        pass
+    train_x, test_x, train_y, test_y = training_splitter(to_model, 0.7)
+    print("Dataset" + str(file) + " split into train/test", sep = "")
+    number_of_tree = np.linspace(1, 501, 26)-1
+    mean_scores, sds = [], []
+    for tree in number_of_tree:
+        if tree == 0:
+            tree = 1
+        else:
+            tree = tree.astype(int)
+        print("Dataset" + str(file) + " tree number: ", str(tree), sep="")
+        #rf_model = random_forest_train(train_x, test_x, tree)
+        #prediction_results = random_forest_predict(rf_model, test_x)
+        #preds = test_y[prediction_results]
+        recogniser = RandomForestClassifier(tree)
+        score = cross_val_score(recogniser, train_x.astype(int), train_y.astype(int))
+        mean_scores.append(np.mean(score))
+        sds.append(np.std(score))
+    mean_array = np.array(mean_scores)
+    sds_array = np.array(sds)
+    plotter(mean_scores, mean_array, sds_array, number_of_tree, file_dir, file)
+    #print("Cross Validation for Dataset", str(file), " beginning...", sep = "")
+    #cv_score = cross_validation(rf_model, test_x, test_y, 10, "recall")
+
+
+    # Split Up Dataset
+    # thg_features = thg_data.loc[:, thg_data.columns != "fraud_status"].astype(int)
+    # thg_target = thg_data['fraud_status'].astype(int)
+    #
+    # # Build the Random Forest Classifier
+    # recogniser = RandomForestClassifier(25)
+    #
+    # # Fit Data
+    # recogniser.fit(thg_features, thg_target)
+    #
